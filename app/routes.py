c934b400c70d9aface1b4cdf7e04e7bf86213aeb
nitinni3 <nitin.goswami@choira.io>
2024-11-28 02:17:42 +0530

    added route to get audio files/file

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,11 @@ main = Blueprint('main', __name__)
 def health_check():
     return jsonify({"status": "API is running!"}), 200
 
+@main.route('/api/audio-files', methods=['GET'])
+def audio_files():
+    return jsonify([]), 200
+
+@main.route('/api/audio/:audio_filename', methods=['GET'])
+def audio_file():
+    return jsonify({"status": "under dev"}), 200
+
