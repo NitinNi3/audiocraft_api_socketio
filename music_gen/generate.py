77ff3060c3cafbb035334dd80604692696c7c73d
nitinni3 <nitin.goswami@choira.io>
2024-11-28 06:23:36 +0530

    duration issue fixing -some test commits

--- music_gen/generate.py
+++ music_gen/generate.py
@@ -12,8 +12,9 @@ class ChoiraGenerate:
         
 
     def generate_music_large(self,prompt,duration,user_socket_id):
+        print("GENERATION STARTED.......")
         self.socket_id = user_socket_id
-        self.model.set_generation_params(duration)
+        self.model.set_generation_params(duration=duration)
         self.model.set_custom_progress_callback(self.progress_callback)
         res = self.model.generate([prompt],progress=True)
         tensor = res.cpu().squeeze(0)
@@ -21,15 +22,14 @@ class ChoiraGenerate:
         sample_rate = 32000  # Replace with your actual sample rate
         file_name = self.generate_filename()
         torchaudio.save(f"audios/{file_name}", tensor, sample_rate)
+        print("GENERATION FINISHED !. File saved as {file_name}")
+
         return file_name
     
     def progress_callback(self,generated,to_generate):
         # hit socket event
         percentage = (generated/to_generate)*100
-        print(f"Percentage:{round(percentage)}%")
-
         self.socketio.emit('music-progress', {'progress':percentage},to=self.socket_id)
-
 
     def generate_filename(self):
         timestamp = int(time.time())
